e6283129ae85623cb11fc83fb35be1d699cfbb31
MayC <mayrautopia@gmail.com>
2023-11-30 14:03:40 -0300

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { StrictMode } from 'react'
-import AuthProvider from './context/AuthProvider.jsx'
-import { ProtectedProvider } from './context/ProtectedProvider.jsx'
-import { PrivateProvider } from './context/PrivateProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
